5bc4e2b7af8f6dc401f5a34f0679c3e60ab7ec87
Ben Brady <benbradybusiness@gmail.com>
2024-02-21 14:19:01 +0000

    Removed SSR

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,47 @@
 <script lang="ts">
+	import { SyncLoader } from 'svelte-loading-spinners';
 	import Metadata from '$lib/components/Metadata.svelte';
 	import PokemonTile from '$lib/components/PokemonTile.svelte';
 	import { listPokemonPaginated, type BasicPokemonInfo } from '$lib/pokemon/index.js';
-	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 
-	let pokemons: BasicPokemonInfo[] = $page.data.pokemon;
+	let pokemons: BasicPokemonInfo[] = [];
 
+	let finished: boolean = false;
 	async function nextPage() {
 		const nextPage = await listPokemonPaginated(pokemons.length);
+		if (nextPage === null) {
+			finished = true;
+			return;
+		}
+
 		pokemons = [...pokemons, ...nextPage];
 	}
+	onMount(nextPage);
 </script>
 
 <Metadata title="Home" description="Pokémon API" />
 
-<section>
+<div id="scroll">
 	{#each pokemons as pokemon}
 		<PokemonTile name={pokemon.name} id={pokemon.id} />
 	{/each}
-</section>
-<button on:click={nextPage}>Next Page</button>
+</div>
+
+<div style="display: contents;" class:visible={!finished} id="loading-icon">
+	<SyncLoader color={"#000000"}/>
+</div>
 
 <style>
-	section {
+	#scroll {
+		height: 100%;
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-around;
 		gap: 1rem;
 	}
 
-	button {
-		width: fit-content;
+	.visible {
+		display: none;
 	}
 </style>
--- src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types.js';
-import { listPokemonPaginated } from '$lib/pokemon/index.js';
-
-export const load: PageLoad = async () => {
-	const pokemon = await listPokemonPaginated();
-	return { pokemon };
-};
