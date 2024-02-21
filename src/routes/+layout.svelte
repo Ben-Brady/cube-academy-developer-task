c3551877c0822f29a0fd12894b180575b4ee10c1
Ben Brady <benbradybusiness@gmail.com>
2024-02-21 15:47:36 +0000

    Updated navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,20 @@
 	import './styles.css';
 	import './reset.css';
 	import PokemonImg from '$lib/images/pokeapi.png';
-	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 
-	const queryClient = new QueryClient()
+	const queryClient = new QueryClient();
 </script>
 
 <QueryClientProvider client={queryClient}>
-	<div>
+	<div id="app">
 		<nav>
 			<a href="/" style="display: contents;">
 				<img src={PokemonImg} alt="PokeAPI" />
 			</a>
+			<div>
+				<a href="https://pokeapi.co">Documentation</a>
+			</div>
 		</nav>
 		<main>
 			<slot />
@@ -21,22 +24,30 @@
 </QueryClientProvider>
 
 <style lang="scss">
-	div {
+	#app {
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
 
 		nav {
 			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			gap: 1rem;
 
 			width: 100%;
-			height: 3rem;
+			height: 3.5rem;
+			padding: 0.25rem;
 			border: 0 solid black;
 			border-bottom-width: 1px;
 			background: var(--accent);
 
 			img {
 				height: 100%;
+			}
+
+			div {
+				padding: 0 1rem;
 			}
 		}
 
