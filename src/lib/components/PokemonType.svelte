47ec1aad87eb5c3f08a697d07b70bb847f460c57
Ben Brady <benbradybusiness@gmail.com>
2024-02-26 16:50:24 +0000

    Added PokemonType element

---
+++ src/lib/components/PokemonType.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	export let type: string;
+</script>
+
+<div data-type={type}>
+	{type}
+</div>
+
+<style lang="scss">
+	div {
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+		box-shadow: var(--shadow-elevation-low);
+
+		border: 0.1rem solid black;
+
+		/* Colours taken from https://pokemondb.net/type */
+		&[data-type='normal'] {
+			background-color: #aa9;
+		}
+		&[data-type='fire'] {
+			background-color: #f42;
+		}
+		&[data-type='water'] {
+			background-color: #39f;
+		}
+		&[data-type='electric'] {
+			background-color: #fc3;
+		}
+		&[data-type='grass'] {
+			background-color: #7c5;
+		}
+		&[data-type='ice'] {
+			background-color: #6cf;
+		}
+		&[data-type='fighting'] {
+			background-color: #b54;
+		}
+		&[data-type='poison'] {
+			background-color: #a59;
+		}
+		&[data-type='ground'] {
+			background-color: #db5;
+		}
+		&[data-type='flying'] {
+			background-color: #89f;
+		}
+		&[data-type='psychic'] {
+			background-color: #f59;
+		}
+		&[data-type='bug'] {
+			background-color: #ab2;
+		}
+		&[data-type='rock'] {
+			background-color: #ba6;
+		}
+		&[data-type='ghost'] {
+			background-color: #66b;
+		}
+		&[data-type='dragon'] {
+			background-color: #76e;
+		}
+		&[data-type='dark'] {
+			background-color: #754;
+		}
+		&[data-type='steel'] {
+			background-color: #aab;
+		}
+		&[data-type='fairy'] {
+			background-color: #e9e;
+		}
+		&[data-type='curse'] {
+			background-color: #698;
+		}
+	}
+</style>
--- src/routes/pokemon/[name]/+page.svelte
+++ src/routes/pokemon/[name]/+page.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	import { getPokemon, type Pokemon } from '$lib/pokemon/index.js';
 	import { SyncLoader } from 'svelte-loading-spinners';
 	import { createQuery } from '@tanstack/svelte-query';
+	import { getPokemon, type Pokemon } from '$lib/pokemon/index.js';
+	import PokemonType from '$lib/components/PokemonType.svelte';
 
 	let { name } = $page.params;
 
 	const query = createQuery({
 		queryKey: [`pokemon-${name}`],
-		queryFn: async () => {return await getPokemon(name)}
+		queryFn: async () => {
+			return await getPokemon(name);
+		}
 	});
-	let pokemon: Pokemon
+	let pokemon: Pokemon;
 	$: pokemon = $query.data;
 	$: types = (() => {
-		if (!pokemon) return []
-		return pokemon.types.map(pokemon_type => pokemon_type.type.name)
-	})()
+		if (!pokemon) return [];
+		return pokemon.types.map((pokemon_type) => pokemon_type.type.name);
+	})();
 </script>
 
 {#if $query.isLoading}
@@ -28,11 +31,22 @@
 	<img src={pokemon.sprites.front_default} alt="$query" />
 	<span>Weight: {pokemon.weight / 10}Kg</span>
 	<span>Height: {pokemon.height / 10}m</span>
-	<span>Types: {types.join(" ")}</span>
+	<span>Types:</span>
+	<div class="types">
+		{#each types as type}
+			<PokemonType {type} />
+		{/each}
+	</div>
 {/if}
 
 <style>
 	h1 {
 		text-transform: capitalize;
+	}
+
+	.types {
+		display: flex;
+		flex-flow: row wrap;
+		gap: 0.5rem;
 	}
 </style>
