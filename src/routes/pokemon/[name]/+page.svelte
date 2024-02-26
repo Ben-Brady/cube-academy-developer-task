18a550e2d883374dcbd2c35b4110c22cbc7b5d07
Ben Brady <benbradybusiness@gmail.com>
2024-02-26 16:43:11 +0000

    Added pokemon page

---
+++ src/routes/pokemon/[name]/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { getPokemon, type Pokemon } from '$lib/pokemon/index.js';
+	import { SyncLoader } from 'svelte-loading-spinners';
+	import { createQuery } from '@tanstack/svelte-query';
+
+	let { name } = $page.params;
+
+	const query = createQuery({
+		queryKey: [`pokemon-${name}`],
+		queryFn: async () => {return await getPokemon(name)}
+	});
+	let pokemon: Pokemon
+	$: pokemon = $query.data;
+	$: types = (() => {
+		if (!pokemon) return []
+		return pokemon.types.map(pokemon_type => pokemon_type.type.name)
+	})()
+</script>
+
+{#if $query.isLoading}
+	<SyncLoader />
+{:else}
+	<h1>
+		{pokemon.name}
+	</h1>
+	<img src={pokemon.sprites.front_default} alt="$query" />
+	<span>Weight: {pokemon.weight / 10}Kg</span>
+	<span>Height: {pokemon.height / 10}m</span>
+	<span>Types: {types.join(" ")}</span>
+{/if}
+
+<style>
+	h1 {
+		text-transform: capitalize;
+	}
+</style>
