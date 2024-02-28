8f32190a89b50fe8b63f9e200cc510df8f4adf87
Ben Brady <benbradybusiness@gmail.com>
2024-02-28 22:47:50 +0000

    Added SearchBar autocomplete

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,9 +1,30 @@
-<script>
+<script lang="ts">
 	import SearchIcon from "$lib/images/search.svg";
+	import { searchPokemon, type BasicPokemonInfo } from "$lib/pokemon";
+	let search = "";
+
+	let autocomplete: BasicPokemonInfo[] = [];
+	$: (async () => {
+		autocomplete = await searchPokemon(search);
+	})();
+
+	let isSearchFocused = true;
+	$: showAutocompleted = isSearchFocused && autocomplete.length > 0;
+
+	let searchBoxWidth = 0;
 </script>
 
-<div class="search">
-	<input class="search-input" />
+<div class="search" bind:clientWidth="{searchBoxWidth}">
+	<input
+		class="search-input"
+		bind:value="{search}"
+		on:focus="{() => {
+			isSearchFocused = true;
+		}}"
+		on:blur="{() => {
+			isSearchFocused = false;
+		}}"
+	/>
 	<div class="search-button">
 		<img
 			width="{25}"
@@ -13,6 +34,20 @@
 			alt=""
 		/>
 	</div>
+
+	<div class="results-container" hidden="{!showAutocompleted}">
+		<div class="results" style="width: {searchBoxWidth}px;">
+			{#each autocomplete as pokemon}
+				<a class="row" href="/pokemon/{pokemon.name}">
+					<img
+						src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon.id}.png"
+						alt="{pokemon.name}"
+					/>
+					<span> {pokemon.name} #{pokemon.id} </span>
+				</a>
+			{/each}
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
@@ -20,14 +55,13 @@
 		display: grid;
 		grid-template-columns: 1fr 3rem;
 
-		height: 100%;
+		height: 2.5rem;
 		width: 25rem;
 		background: #d9d9d9;
 		border-radius: 1rem;
-		overflow: hidden;
 
 		&:has(input:focus) {
-			border: var(--text) solid 0.1rem;
+			outline: var(--text) solid 0.1rem;
 		}
 
 		.search-input {
@@ -36,6 +70,10 @@
 			height: 100%;
 			width: 100%;
 			padding: 0 1rem;
+
+			&:focus {
+				outline: none;
+			}
 		}
 
 		.search-button {
@@ -47,6 +85,30 @@
 			display: flex;
 			justify-content: center;
 			align-items: center;
+		}
+
+		.results-container {
+			position: absolute;
+			margin-top: 3rem;
+
+			.results {
+				width: fit-content;
+				min-width: 10rem;
+				height: 100%;
+				border-radius: 0.5rem;
+				background: var(--accent);
+
+				.row {
+					display: flex;
+					gap: 1rem;
+					height: 2rem;
+					padding: 0 0.5rem;
+
+					img {
+						height: 100%;
+					}
+				}
+			}
 		}
 	}
 </style>
