2d8fc4c5f64a675df3b5efcfc0b088970af17eb2
Ben Brady <benbradybusiness@gmail.com>
2024-02-29 16:26:13 +0000

    Pokemon page no longer uses SSR

--- src/routes/pokemon/[name]/+page.svelte
+++ src/routes/pokemon/[name]/+page.svelte
@@ -1,14 +1,43 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
 	import { page } from "$app/stores";
-	import PokemonInfo from "$lib/components/PokemonPage/index.svelte";
+	import PokemonPage from "$lib/components/PokemonPage/index.svelte";
 	import Metadata from "$lib/components/Metadata.svelte";
+	import { SyncLoader } from "svelte-loading-spinners";
+	import { getPokemon } from "$lib/pokemon";
+	import { formatPokemonName } from "$lib/format";
+	import { createQuery } from "@tanstack/svelte-query";
 
-	let { pokemon } = $page.data;
-	const capatalise = (text: string) => text
-		.split(" ")
-		.map(word => word[0].toUpperCase() + word.substring(1))
-		.join(" ");
+	let { name } = $page.params;
+
+	const query = createQuery({
+		queryKey: ["pokemon", name],
+		queryFn: async () => {
+			const pokemon = await getPokemon(name);
+			if (!pokemon) throw new Error("Not found");
+			return pokemon;
+		},
+		retry: false,
+	});
 </script>
 
-<Metadata title="{capatalise(pokemon.name)}" description="Pokémon API" />
-<PokemonInfo {pokemon} />
+{#if $query.isSuccess}
+	<Metadata title="{formatPokemonName($query.data.name)}" description="Pokémon API" />
+	<PokemonPage pokemon="{$query.data}" />
+{:else if $query.isFetching}
+	<div class="center">
+		<SyncLoader color="var(--text)" />
+	</div>
+	{:else}
+	{goto("/", { replaceState: true })}
+	{/if}
+
+<style>
+	.center {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+	}
+</style>
